7c962885ca45e08d9f43d1f2ee148fcb790e77f1
cefariron <adidaswww17@gmail.com>
2024-03-03 20:45:25 +0200

    1

---
+++ src/components/EditForm/EditForm.jsx
@@ -0,0 +1,73 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useDispatch } from "react-redux";
+import { addContact } from "../../redux/operations";
+import { useId } from "react";
+import css from "../EditForm/EditForm.module.css";
+// import * as Yup from "yup";
+
+// const validationSchema = Yup.object().shape({
+//   name: Yup.string()
+//     .min(3, "Enter 3 to 50 characters")
+//     .max(50, "Enter 3 to 50 characters")
+//     .required("Field cant be empty!"),
+//     number: Yup.string()
+//     .min(3, "Enter 3 to 15 characters")
+//     .max(15, "Enter 3 to 15 characters")
+//     .required("Field cant be empty!"),
+// });
+
+export const EditForm = () => {
+  const dispatch = useDispatch();
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const initialValues = {
+    name: "",
+    number: "",
+  };
+
+  const handleSubmit = (values, actions) => {
+    dispatch(addContact(values));
+    actions.resetForm();
+  };
+
+  return (
+    <>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        // validationSchema={validationSchema}
+      >
+        <Form className={css.form}>
+          <h3 className={css.title}>Add contact</h3>
+          <div className={css.inputContainer}>
+            <label className={css.label} htmlFor={nameFieldId}>
+              Name
+            </label>
+            <Field className={css.input} id={nameFieldId} name="name"/>
+            <ErrorMessage className={css.error} name="name" as="span" />
+          </div>
+          <div className={css.inputContainer}>
+            <label className={css.label} htmlFor={numberFieldId}>
+              Number
+            </label>
+            <Field className={css.input} id={numberFieldId} name="number" />
+            <ErrorMessage className={css.error} name="number" as="span" />
+          </div>
+          <div className={css.option}>
+            <div>
+              <label className={css.checkboxContainer}>
+                <input type="checkbox" id="rememberMeCheckbox" />
+                <span className={css.checkmark}></span>
+                Sync with all devices
+              </label>
+            </div>
+          </div>
+          <button className={css.btn} type="submit">
+            Add contact
+          </button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,9 +10,12 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import { FaRegEdit } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 import css from "./Modal.module.css";
+import { useState } from "react";
+import { EditForm } from "../EditForm/EditForm";
 
 export const Modal = () => {
   const dispatch = useDispatch();
+  const [isEditNow, setIsEditNow] = useState(false);
 
   const isModalOpen = useSelector(selectModal);
   const selectedContactId = useSelector(selectContactIdForModal);
@@ -23,11 +26,6 @@ export const Modal = () => {
   const closeModal = () => {
     dispatch(closeContactModal());
   };
-
-  // const handleRemoveModal = () => {
-  //   dispatch(closeContactModal());
-  //   dispatch(deleteContact(selectedContactId))
-  // };
 
   const handleRemove = () => {
     dispatch(deleteContact(selectedContactId));
@@ -40,32 +38,35 @@ export const Modal = () => {
       className={css.modalOverlay}
     >
       {selectedContact && (
-        <div className={css.container}>
-          <div className={css.userInfoContainer}>
-            <img
-              className={css.avatar}
-              src="src\components\Modal\avatar.png"
-              alt="avatar"
-            />
-            <div className={css.contactInfo}>
-              <div>
-                <p className={css.userName}>Name: {selectedContact.name}</p>
-                <p>Number:</p>
-                <p>{selectedContact.number}</p>
+        <div className={css.wrapper}>
+          <div className={css.container}>
+            <div className={css.userInfoContainer}>
+              <img
+                className={css.avatar}
+                src="src\components\Modal\avatar.png"
+                alt="avatar"
+              />
+              <div className={css.contactInfo}>
+                <div>
+                  <p className={css.userName}>Name: {selectedContact.name}</p>
+                  <p>Number:</p>
+                  <p>{selectedContact.number}</p>
+                </div>
               </div>
             </div>
+            <div className={css.btnContainer}>
+              <button className={css.btn} onClick={closeModal}>
+                <IoMdClose size={20} />
+              </button>
+              <button className={css.btn} type="button">
+                <FaRegEdit className={css.btnIcon} size={16} onClick={setIsEditNow}/>
+              </button>
+              <button className={css.btn} type="button" onClick={handleRemove}>
+                <RiDeleteBin6Line className={css.btnIcon} size={18} />
+              </button>
+            </div>
           </div>
-          <div className={css.btnContainer}>
-            <button className={css.btn} onClick={closeModal}>
-              <IoMdClose size={20} />
-            </button>
-            <button className={css.btn} type="button">
-              <FaRegEdit className={css.btnIcon} size={16} />
-            </button>
-            <button className={css.btn} type="button" onClick={handleRemove}>
-              <RiDeleteBin6Line className={css.btnIcon} size={18} />
-            </button>
-          </div>
+          {isEditNow && <EditForm id={selectedContact.id} name={selectedContact.name} number={selectedContact.number}/>}
         </div>
       )}
     </div>
