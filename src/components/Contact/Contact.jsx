d0fd3bb9c0b815ac462147b7d524586614d0a962
cefariron <adidaswww17@gmail.com>
2024-03-04 09:00:16 +0200

    1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import { deleteContact } from "../../redux/operations";
 import { LuPhone } from "react-icons/lu";
 import { RiContactsLine } from "react-icons/ri";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { FaRegEdit } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 import css from "../Contact/Contact.module.css";
 
 export const Contact = ({ id, name, number, handleContactClick }) => {
@@ -22,14 +22,14 @@ export const Contact = ({ id, name, number, handleContactClick }) => {
           <p className={css.name}>
             <RiContactsLine className={css.iconInfo} /> {name}
           </p>
-          <p>
+          <p className={css.number}>
             <LuPhone className={css.iconInfo} /> {number}
           </p>
         </div>
       </div>
       <div className={css.btnContainer}>
-        <button className={css.btn} type="button">
-          <FaRegEdit className={css.icon} />
+        <button className={css.btn} type="button" onClick={() => handleContactClick(id)}>
+          <RxHamburgerMenu className={css.icon} />
         </button>
         <button className={css.btn} type="button" onClick={handleRemove}>
           <RiDeleteBin6Line className={css.icon} />
